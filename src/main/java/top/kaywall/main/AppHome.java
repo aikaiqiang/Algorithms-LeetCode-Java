b89b958299948c8c7f50669617a725c6e0bf281d
aikaiqiang <akqdream@gmail.com>
2021-11-06 03:22:41 +0800

    【修改原因】调整目录结构 【修改内容】新增目录，添加 .gitignore

---
+++ src/main/java/top/kaywall/main/AppHome.java
@@ -0,0 +1,16 @@
+package top.kaywall.main;
+
+
+/**
+ *
+ * @desc
+ * @author Kaiqiang Ai
+ * @addTime 2021/11/06 03:20:54
+ */
+public class AppHome {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");   
+    }
+
+}
